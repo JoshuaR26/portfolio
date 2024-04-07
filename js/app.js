f70c920834f438eb0b682f675cc29373e80f0bac
JoshuaR26 <joshua2009474@gmail.com>
2024-04-08 01:22:47 +0530

    fix: buffer commit

--- js/app.js
+++ js/app.js
@@ -5,17 +5,3 @@ document.addEventListener('DOMContentLoaded', (e) =>{
         splash.classList.add('display-none');
     }, 2000);
 })
-
-
-function setNewImg() {
-    var img = document.getElementById('home-img');
-    img.src = '../images/home.png';
-    img.style.opacity = '1'; // Change opacity property
-}
-
-function setOldImg() {
-    var img = document.getElementById('home-img');
-    img.src = '../images/home_bw.png';
-    img.style.opacity = '1'; // Change opacity property
-    img.style.transition = 'opacity 1s ease'; // Apply transition property
-}
